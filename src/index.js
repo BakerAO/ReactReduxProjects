2d52d3db2c5baa8ae33aa6cb8b65ca9f2b8d3c24
BakerAO <dpshunter@gmail.com>
2018-01-27 17:55:07 -0600

    Setting up dependencies and src/index.js
    Signed-off-by: BakerAO <dpshunter@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+
+import reducers from './reducers';
+import PostsIndex from './blog/components/posts_index';
+import PostsNew from './blog/components/posts_new';
+import PostsShow from './blog/compoents/posts_show';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path="/posts/new" component={PostsNew} />
+                    <Route path="/posts/:id" component={PostsShow} />
+                    <Route path="/posts/" component={PostsIndex} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>,
+    document.querySelector('.container')
+);
